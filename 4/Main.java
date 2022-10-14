5619c03290c821cc0fb31d194018c81f584854e5
ryo mitsuda <s2132134lj@s.chibakoudai.jp>
2022-10-14 12:28:03 +0900

    KANSEI

--- 4/Main.java
+++ 4/Main.java
@@ -1,6 +1,24 @@
+import java.util.*;
 class Main {
     public static void main(String[] args) {
-        System.out.println(-log2(1.0/4.0));
+        Scanner scan = new Scanner(System.in);
+        System.out.print("length=> ");
+        int n = scan.nextInt();
+
+        int sum = 0;
+        double ans = 0,arr[] = new double[n];
+        
+        for(int i = 0; i < n; i++) {
+            System.out.print("num" + (i+1) + "=> ");
+            arr[i] = scan.nextInt();
+            sum += arr[i];
+        }
+        
+        for(int i = 0; i < n; i++) {
+            ans = ans + ((-arr[i]/sum) * log2((arr[i]/sum)));
+        }
+        System.out.println("-------");
+        System.out.println("entropy = " + ans);
     }
     static double log2(double x) {
         return (Math.log(x) / Math.log(2));
